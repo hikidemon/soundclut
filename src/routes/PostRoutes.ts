b2782408229fc5fe78d7afb6a5222782b1a35753
hikidemon <nikyurkov2005@gmail.com>
2024-10-27 22:08:27 +0700

    2

--- src/routes/PostRoutes.ts
+++ src/routes/PostRoutes.ts
@@ -1,9 +1,21 @@
 import express, { Request, Response, Router } from 'express';
 import Post from '../models/Post';
 import Like from '../models/Like';
-import Comment from '../models/Comment'; // Убедитесь, что это импортировано
-const router: Router = express.Router();
+import Comment from '../models/Comment'; 
+import User from '../models/User';
+const router = express.Router();
 
-// Лайк поста
+router.get('/posts', async (req: Request, res: Response) => {
+  try {
+    const posts = await Post.findAll({
+      include: [{ model: User, as: 'user', attributes: ['username'] }],
+      attributes: ['id', 'title', 'content', 'musicFileUrl', 'likesCount', 'repostCount', 'createdAt']
+    });
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error('Error fetching posts:', error); 
+    res.status(500).json({ message: 'Internal Server Error',error});
+  }
+});
 
 export default router;
